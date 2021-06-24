6f4a048a34720cec419617df4b4c428096ab070a
Shiliang <shiliang1992@outlook.com>
2021-06-24 17:14:50 +0800

    winit test

--- src/bin/main2.rs
+++ src/bin/main2.rs
@@ -1,17 +1,26 @@
 
 /**
 */
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
 fn main() {
 
-    let some_u8_value = 18u8;
-    match some_u8_value {
-        1 => println!("one"),
-        3 => println!("three"),
-        5 => println!("five"),
-        7 => println!("seven"),
-        _ => {
-            println!("none:{:?}", some_u8_value)
-        },
-    }
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+            _ => (),
+        }
+    });
 
 }
