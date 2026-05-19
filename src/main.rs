1240f73abae2db934320aeb3dbadc570bb7807ee
石良 <ext.danliang6@jd.com>
2026-05-19 19:33:17 +0800

    feat(main): 添加结构体定义和打印功能
    - 注释掉原有的模块导入
    - 定义 Person 结构体并实现 Debug trait
    - 添加 hello_world 函数用于基本打印
    - 添加 test_println 函数测试结构体打印功能
    - 实现结构体的调试打印和字段访问
    - 调整主函数调用新的打印函数

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,39 @@
-mod test_gtk;
-mod test1_1;
-mod GPUI_Component_Test;
+// mod test_gtk;
+// mod test1_1;
+// mod GPUI_Component_Test;
 // mod test2_1;
 // mod test2_2;
 // mod test2_3;
 // mod test4_3;
 // use test2_2 as test;
-
+#[derive(Debug)]
+struct Person<'a> {
+    name: &'a str,
+    age: u8
+}
 fn main() {
+    hello_world();
+    test_println();
     // test1_1::test1();
     // test2_1::test1();
     // test::test2();
     // test2_3::test3();
     // test_gtk::test_gtk();
-    GPUI_Component_Test::main();
+    // GPUI_Component_Test::main();
+}
+fn hello_world() {
+    println!("Hello, World!");
+    println!("{} days", "31");
+}
+fn test_println() {
+    let name = "Peter";
+    let age = 27;
+    let peter = Person { name, age };
+
+    // Pretty print
+    println!("{:?}", peter);
+    // Pretty print
+    println!("{:#?}", peter);
+    // 访问字段
+    println!("Name: {}, Age: {}", peter.name, peter.age);
 }
